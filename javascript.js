b00a42b00ced94179781d8427d18db459b9c6992
KevinAcz <kevinaceitonsalazar@gmail.com>
2023-08-16 18:01:26 -0400

    Added grid selector button

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 const gridContainer = document.querySelector ('#grid-container');
 
+//Default grid
 let columnNumber = 16;
 let rowNumber = 16;
+createGrid(columnNumber, rowNumber);
 
 function createGrid (columnNumber, rowNumber) {
     let i = 0;
@@ -23,19 +25,35 @@ function createGrid (columnNumber, rowNumber) {
         j = 0; 
     }
 }
-
-
-createGrid(columnNumber, rowNumber)
-
-
-const resetButton = document.querySelector('#reset-btn');
-
-resetButton.addEventListener('click', function() {
+function deleteGrid() {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
+}
+
+
+const inputButton = document.querySelector('#input-btn');
+const resetButton = document.querySelector('#reset-btn');
+
+inputButton.addEventListener('click', function() {
+    columnNumber = prompt('Columnas');
+    while (isNaN(columnNumber) || columnNumber > 100) {
+        columnNumber = prompt('Not a valid number! Enter columns between 1 to 100: ');
+    }
+    rowNumber = prompt('Filas');
+    while (isNaN(rowNumber) || rowNumber > 100) {
+        rowNumber = prompt('Not a valid number! Enter rows between 1 to 100: ');
+    }
+
+    deleteGrid();
+    createGrid(columnNumber,rowNumber);
+});
+
+resetButton.addEventListener('click', function() {
+
+    deleteGrid();
 
     createGrid(columnNumber,rowNumber);
 
     
-})
+});
